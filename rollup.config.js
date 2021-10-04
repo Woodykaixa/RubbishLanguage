09d5ab6a30e3d7a3e6c50887d44a5fee05d26ba2
kaixa <690750353@qq.com>
2021-10-04 22:38:41 +0800

    chore(rollup): resolve json file

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'rollup';
-import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import pkg from './package.json';
 import alias from '@rollup/plugin-alias';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
 import path from 'path';
+import { defineConfig } from 'rollup';
+import pkg from './package.json';
 
 const projectRootDir = path.resolve(__dirname);
 const extensions = ['.js', '.ts'];
@@ -24,6 +25,7 @@ const config = defineConfig({
         },
       ],
     }),
+    json(),
     resolve({ extensions, modulesOnly: true }),
     commonjs(),
     babel({ babelHelpers: 'bundled', include: ['src/**/*'], extensions }),
